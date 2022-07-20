39ae6b61ce72ef4ad816a4184f84aa11ff48a957
Pedro César <pedrodhin@gmail.com>
2022-07-20 17:54:52 -0300

    Projeto MakeYourBurger Vue3

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer id="footer">
+        <p>Make Your Burguer &copy; 2022</p>
+    </footer>
+</template>
+<script>
+export default {
+    name: "MeuFooter"
+}
+</script>
+
+<style scoped>
+#footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom:0;
+    width: 100%;
+}
+</style>
